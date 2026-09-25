613059a727f3e333a9b04a8d64bfe15d344af176
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2026-09-25 11:27:24 +0200

    ✨ [vtb-4] feat(chore): log in and use ApiProperty for the API example.

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,9 +1,16 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
   @IsEmail({}, { message: 'email must be a valid email address' })
+  @ApiProperty({
+    description: 'The email of the user',
+    example: 'lucas.martinie@gmail.com'})
   email: string;
 
   @IsNotEmpty({ message: 'password is required' })
+  @ApiProperty({
+    description: 'The password of the user',
+    example: 'lucas123'})
   password: string;
 }
